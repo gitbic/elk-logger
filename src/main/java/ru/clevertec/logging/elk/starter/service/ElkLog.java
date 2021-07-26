e015132c5522143fdc89ca57ae26d3a6a3b5fb04
pturskiy@clevertec.ru <pturskiy@clevertec.ru>
2021-07-26 10:38:02 +0300

    change ElkLog storage from HashMap to LinkedHashMap; added remove methods for message, request and response

--- src/main/java/ru/clevertec/logging/elk/starter/service/ElkLog.java
+++ src/main/java/ru/clevertec/logging/elk/starter/service/ElkLog.java
@@ -8,28 +8,33 @@ import uk.org.lidalia.slf4jext.Level;
 import uk.org.lidalia.slf4jext.Logger;
 import uk.org.lidalia.slf4jext.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static net.logstash.logback.argument.StructuredArguments.keyValue;
 
 @Slf4j
 public class ElkLog {
-    private final Map<String, Object> loggingFieldsMap;
 
+    private final Map<String, Object> loggingFieldsMap;
     private final Logger logger;
     private Level level;
 
-    public ElkLog(String loggerName) {
-        loggingFieldsMap = new HashMap<>();
-        logger = LoggerFactory.getLogger(loggerName);
+    public ElkLog(String loggingClassName) {
+        loggingFieldsMap = new LinkedHashMap<>();
+        logger = LoggerFactory.getLogger(loggingClassName);
         level = Level.INFO;
+    }
+
+    public ElkLog(Class<?> clazz) {
+        this(clazz.getName());
     }
 
     public static ElkLog builder(String loggerName) {
         return new ElkLog(loggerName);
+    }
+
+    public static ElkLog builder(Class <?> clazz) {
+        return builder(clazz.getName());
     }
 
     public ElkLog setLevel(Level level) {
@@ -42,7 +47,7 @@ public class ElkLog {
         return this;
     }
 
-    public ElkLog addLoggerName(String loggerName) {
+    public ElkLog setLoggerName(String loggerName) {
         addField(ElasticFieldName.LOGGER_NAME, loggerName);
         return this;
     }
@@ -51,12 +56,41 @@ public class ElkLog {
         return addField(ElasticFieldName.MESSAGE, message);
     }
 
+    public ElkLog setMessage(String message) {
+        return addField(ElasticFieldName.MESSAGE, message);
+    }
+
+    public ElkLog setField(String fieldName, Object fieldValue) {
+        loggingFieldsMap.put(fieldName, fieldValue);
+        return this;
+    }
+
     public ElkLog addRequest(Object request) {
         return addField(ElasticFieldName.REQUEST, request);
     }
 
     public ElkLog addResponse(Object response) {
         return addField(ElasticFieldName.RESPONSE, response);
+    }
+
+    public ElkLog removeMessage() {
+        loggingFieldsMap.remove(ElasticFieldName.MESSAGE);
+        return this;
+    }
+
+    public ElkLog removeRequest() {
+        loggingFieldsMap.remove(ElasticFieldName.REQUEST);
+        return this;
+    }
+
+    public ElkLog removeResponse() {
+        loggingFieldsMap.remove(ElasticFieldName.RESPONSE);
+        return this;
+    }
+
+    public ElkLog removeField(String fieldName) {
+        loggingFieldsMap.remove(fieldName);
+        return this;
     }
 
     public void print() {
@@ -69,12 +103,11 @@ public class ElkLog {
         logger.log(level, parentheses, loggingFields);
     }
 
-
     public void printToElk() {
         logger.log(level, Constants.EMPTY_STRING, getLoggingFields());
     }
 
-        public void printToConsole() {
+    public void printToConsole() {
         logger.log(level, Constants.PARENTHESES, Arrays.asList(getLoggingFields()));
     }
 
@@ -86,9 +119,6 @@ public class ElkLog {
     }
 
 
-
-    // todo loggerName
-    // todo custom fields from yml
     // todo readme
     // todo Mishe
 }
